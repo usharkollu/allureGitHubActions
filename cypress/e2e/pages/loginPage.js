f3ea78f4cc096fc5e07917397dbf8e53dea15729
usharkollu <usharkollu@users.noreply.github.com>
2023-01-21 04:05:57 +0000

    Automated deployment: Sat Jan 21 04:05:57 UTC 2023 5abc379cb8fd820366655ead35232db76bffea70

--- cypress/e2e/pages/loginPage.js
+++
@@ -1,7 +0,0 @@
-class LoginPage {
-    getUserName = () => cy.get('#user-name');
-    getUserPassword = () => cy.get('#password');
-    getSubmitButton = () => cy.get('#login-button');
-    
-    }
-    module.exports = new LoginPage();
